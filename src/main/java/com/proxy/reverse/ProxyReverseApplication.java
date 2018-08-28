af55fc30dd2af0d4d3e5746749b5d8c12787effc
fabriciofelipe <fabriciooak@gmail.com>
2018-08-28 10:52:07 -0300

    first commit

---
+++ src/main/java/com/proxy/reverse/ProxyReverseApplication.java
@@ -0,0 +1,12 @@
+package com.proxy.reverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyReverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyReverseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/proxy/reverse/ProxyReverseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.proxy.reverse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProxyReverseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
