553dca631fcec04b4b73bc61dfa64f14995157e2
fabriciofelipe <fabriciooak@gmail.com>
2018-08-28 12:22:33 -0300

    auto-signed key creation and jetty configuration for support and testing to check if ssl is enabled

--- src/test/java/com/proxy/reverse/ProxyReverseApplicationTests.java
+++ src/test/java/com/proxy/reverse/ProxyReverseApplicationTests.java
@@ -2,15 +2,23 @@ package com.proxy.reverse;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.AbstractConfigurableWebServerFactory;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProxyReverseApplicationTests {
 
+	@Autowired
+	private AbstractConfigurableWebServerFactory webServerFactory;
+
 	@Test
-	public void contextLoads() {
+	public void testSsl() {
+		assertThat(this.webServerFactory.getSsl().isEnabled()).isTrue();
 	}
 
 }
