1b19c01f6bb67bf2a09d9473cd8d03bd2fe7df3e
fabriciofelipe <fabriciooak@gmail.com>
2018-08-29 01:01:58 -0300

    implementation of the service class that builds the Content URL that will receive the requests and tests of this class of service

---
+++ src/main/java/com/proxy/reverse/service/WebServerService.java
@@ -0,0 +1,73 @@
+package com.proxy.reverse.service;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.client.api.Request;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+import java.util.Map.Entry;
+
+@Service
+@Slf4j
+@Data
+public class WebServerService
+{
+	private HttpClient httpClient;
+	private String serverName;
+	private Integer port;
+
+	public WebServerService(String serverName, Integer port) throws Exception
+	{
+		this.httpClient = new HttpClient();
+		this.serverName = serverName;
+		this.port = port;
+	}
+
+	public ContentResponse getContentResponse(String target, HttpServletRequest request) throws Exception{
+
+		log.info("target {}", target);
+
+		if("GET".equals(request.getMethod()))
+		{
+			return doGet(target);
+		}
+		
+		return doPost(target, request);
+	}
+
+	public ContentResponse doPost(String target, HttpServletRequest request) throws Exception
+	{
+		String contentURL = getContentURL();
+		log.info("Content URL doPost {}", contentURL);
+
+		Map<String, String[]> parameters = request.getParameterMap();
+		Request postRequest = httpClient.POST(contentURL);
+		for (Entry<String, String[]> parameter : parameters.entrySet())
+		{
+			postRequest = postRequest.param(parameter.getKey(), String.join(",", parameter.getValue()));
+		}
+		return postRequest.send();
+	}
+
+	public ContentResponse doGet(String target) throws Exception
+	{
+		String contentURL = getContentURL();
+		log.info("Content URL doGet {}", contentURL);
+		return httpClient.GET(contentURL);
+	}
+
+	public String getFQDN()
+	{
+		return serverName;
+	}
+	
+	public String getContentURL()
+	{
+		return "http://" + getFQDN() + ":" + port;
+	}
+}
--- src/test/java/com/proxy/reverse/ProxyReverseApplicationTests.java
+++ src/test/java/com/proxy/reverse/ProxyReverseApplicationTests.java
@@ -1,11 +1,16 @@
 package com.proxy.reverse;
 
+import com.proxy.reverse.service.WebServerService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.AbstractConfigurableWebServerFactory;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.servlet.http.HttpServletRequest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,9 +21,50 @@ public class ProxyReverseApplicationTests {
 	@Autowired
 	private AbstractConfigurableWebServerFactory webServerFactory;
 
+
+
 	@Test
 	public void testSsl() {
 		assertThat(this.webServerFactory.getSsl().isEnabled()).isTrue();
 	}
 
+
+	@Test
+	public void testGetContentURL() throws Exception
+	{
+		WebServerService webServer = new WebServerService("10.0.0.2", 8000);
+		String expected = "http://10.0.0.2:8000";
+
+		String actual = webServer.getContentURL();
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testGetContentResponse_RequestGetMethod() throws Exception
+	{
+
+		HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+		Mockito.when(request.getMethod()).thenReturn("GET");
+
+		WebServerService server = Mockito.mock(WebServerService.class);
+		Mockito.doCallRealMethod().when(server).getContentResponse(Mockito.any(), Mockito.any());
+
+		server.getContentResponse(null, request);
+
+		Mockito.verify(server, Mockito.times(1)).doGet(null);
+	}
+	@Test
+	public void testGetContentResponse_RequestPostMethod() throws Exception
+	{
+		HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+		Mockito.when(request.getMethod()).thenReturn("POST");
+
+		WebServerService server = Mockito.mock(WebServerService.class);
+		Mockito.doCallRealMethod().when(server).getContentResponse(Mockito.any(), Mockito.any());
+
+		server.getContentResponse(null, request);
+
+		Mockito.verify(server, Mockito.times(1)).doPost(null, request);
+	}
 }
